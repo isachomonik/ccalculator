bab0845b737312640886aaf5c0dea258c131a87b
isachomonik <dev.isachomonik@gmail.com>
2023-07-28 21:54:20 -0300

    first commit

--- calculator.c
+++ calculator.c
@@ -4,6 +4,15 @@ void main()
 {
     float num1, num2, res;
     int op;
+    char choice;
+
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("      Welcome to the calc'o'matic 1.0      \n");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+
+    do
+    {
 
     printf("Enter your FIRST number :\n");
     scanf("%f", &num1);
@@ -20,10 +29,45 @@ void main()
 
     scanf("%d", &op);
 
-    if(op == 1){
+    switch(op)
+    {
+    case 1:
         res = num1 + num2;
-        printf("%f", res);
+        printf("%f\n", res);
+        break;
+    case 2:
+        res = num1 - num2;
+        printf("%f\n", res);
+        break;
+    case 3:
+        res = num1 * num2;
+        printf("%f\n", res);
+        break;
+    case 4:
+        if(num1 == 0 || num2 == 0)
+        {
+            printf("Impossivel dividir por zero!\n");
+            break;
+        }
+        res = num1 / num2;
+        printf("%f\n", res);
+        break;
+    default:
+        printf("Invalid choice!\n");
     };
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+    while ((getchar()) != '\n');
+
+    printf("Do you want to make another operation? (Type '1' or 'Y' to continue and '2' or 'N' to stop.): \n");
+    scanf("%c", &choice);
+
+    } while(choice == '1' || choice == 'y' || choice == 'Y');
+
+    printf("Thank you for using the calculator!\n");
+
+
+
 
     return 0;
 };
